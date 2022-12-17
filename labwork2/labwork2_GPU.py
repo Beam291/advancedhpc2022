b079b8bef66ee58fc209e0195cd6ffe9b4fc5a1a
Beam291 <khoinl291@gmail.com>
2022-12-17 17:58:27 +0700

    update

---
+++ labwork2/labwork2_GPU.py
@@ -0,0 +1,11 @@
+from numba import cuda
+from numba.cuda.cudadrv import enums
+
+
+# print(cuda.detect())
+device = cuda.get_current_device()
+attribs= [name.replace("CU_DEVICE_ATTRIBUTE_", "") for name in dir(enums) if name.startswith("CU_DEVICE_ATTRIBUTE_")]
+# for attr in attribs:
+    # print(attr, '=', getattr(device, attr))
+    
+print(getattr(device, 'MULTIPROCESSOR_COUNT'))
