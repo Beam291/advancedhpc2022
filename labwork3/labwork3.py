7bd14cbf7473b0c7862c8d85ecbeae708a7ccd8c
Beam291 <khoinl291@gmail.com>
2022-12-20 15:28:40 +0700

    update

--- labwork3/labwork3.py
+++ labwork3/labwork3.py
@@ -1,15 +1,15 @@
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-from PIL import Image
 from numba import cuda, jit
 from timeit import default_timer as timer
-
+import warnings
+warnings.filterwarnings("ignore")
 
 # def grayScale(src, dst):
-    # tidx = cuda.threadIdx.x + cuda.blockIdx * cuda.blockDim.x
-    # g = np.uint8(src[tidx, 0] + src[tidx, 1] + src[tidx, 2] / 3)
-    # dst[tidx, 0] = dst[tidx, 1] = dst[tidx, 2] = g
+#     tidx = cuda.threadIdx.x + cuda.blockIdx * cuda.blockDim.x
+#     g = np.uint8(src[tidx, 0] + src[tidx, 1] + src[tidx, 2] / 3)
+#     dst[tidx, 0] = dst[tidx, 1] = dst[tidx, 2] = g
     
 def grayScale_noGPU(imgArray : np.ndarray):
   for i in imgArray:
@@ -18,7 +18,7 @@ def grayScale_noGPU(imgArray : np.ndarray):
   imgGray = imgArray.reshape(width, height, 3)
   return imgGray
 
-@jit(target_backend='cuda')
+@jit(target_backend ='cuda')
 def grayScale_GPU(imgArray : np.ndarray):
   for i in imgArray:
     gray = ((i[0]+i[1]+[2])/3)
@@ -27,52 +27,30 @@ def grayScale_GPU(imgArray : np.ndarray):
   return imgGray
     
 fileName = 'labimg.jpg'
+# fileName = '102368197_p0.jpg'
 
 filePath = "img/"+fileName
 
-im = Image.open(filePath)
-width, height = im.size
-im.close()
-
-hostInput = np.zeros((height, width, 3), np.uint8)
-devOutput = cuda.device_array((height, width, 3), np.uint8)
-devData = cuda.to_device(hostInput)
+img = mpimg.imread(filePath)
+imgShape = np.shape(img)
+width, height = imgShape[0], imgShape[1]
 
 pixelCount = width * height
 blockSize = 64
 gridSize  = pixelCount / blockSize
+gridSize = round(gridSize)
 
-img = mpimg.imread(filePath)
+# hostInput = np.zeros((height, width, 3), np.uint8)
+# devOutput = cuda.device_array((height, width, 3), np.uint8)
+# devData = cuda.to_device(hostInput)
 
 flatten = img.flatten().reshape(pixelCount,3)
 
 start = timer()
 imggray1 = grayScale_noGPU(flatten)
-# print("Without GPU: ", timer() - start())
+print("Without GPU: ", timer() - start)
 
 start = timer()
 imggray2 = grayScale_GPU(flatten)
-# print("With GPU: ", timer() - start())
+print("With GPU: ", timer() - start)
 
-
-# print(img)
-# print(flatten)
-# imgplot = plt.imshow(devInput)
-# plt.show()
-
-# t1 = time.time()
-
-
-# for i in flatten:
-#   gray = ((i[0]+i[1]+[2])/3)
-#   i[0], i[1], i[2] = gray, gray, gray
-
-# t2 = time.time()
-
-# print(t2-t1)
-
-# imgray1 = flatten.reshape(width, height, 3)
-# imgplot = plt.imshow(imgray1)
-# plt.show()
-
-# print(grayScale[gridSize, blockSize](devInput, devOutput))
